67ca40ebdfaca0b17db9e840e0d9c96f0ca83f93
Souvik Sanyal <71000257+Souvik00@users.noreply.github.com>
2024-05-26 06:07:26 +0600

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+int main()
+{
+   pid_t pid1, pid2, pid3;
+   int a = 10, b = 5;
+   prctl(PR_SET_NAME, "Pareent", 0, 0, 0);
+   printf("Parent process (PID: %d)\n", getpid());
+   pid1 = fork();
+   if (pid1 == 0)
+   {
+      prctl(PR_SET_NAME, "child_1", 0, 0, 0);
+      printf("Child 1 (PID: %d)\n", getpid());
+      printf("Child 1 : Addition %d + %d = %d\n", a, b, a + b);
+      return 0;
+   }
+  wait(NULL);
+   pid2 = fork();
+   if (pid2 == 0)
+   {
+      prctl(PR_SET_NAME, "child_2", 0, 0, 0);
+      printf("Child 2 (PID: %d)\n", getpid());
+      printf("Child 2 : Subtraction %d - %d = %d\n", a, b, a - b);
+      return 0;
+   }
+  wait(NULL);
+   pid3 = fork();
+   if (pid3 == 0)
+   {
+      prctl(PR_SET_NAME, "child_3", 0, 0, 0);
+      printf("Child 3 (PID: %d)\n", getpid());
+      printf("Child 3 : Multiplication %d * %d = %d\n", a, b, a * b);
+      return 0;
+   }
+   printf("Parent process ends\n");
+   sleep(100);
+   return 0;
+}
+
+
